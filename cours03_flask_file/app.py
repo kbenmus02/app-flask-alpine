fbda5b4561cbcd800492ec64bd029b05331875dd
kbenmus02 <mkbensmaia@gmail.com>
2022-01-19 17:05:10 -0500

    fixe code

--- cours03_flask_file/app.py
+++ cours03_flask_file/app.py
@@ -198,4 +198,4 @@ def load_model(model_file_info: str):
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", 5000)
-    app.run(debug=False, host="0.0.0.0", port = por
+    app.run(debug=False, host="0.0.0.0", port = port)
