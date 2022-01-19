af559f8e6d412a7576d2a8d32e9ffa94137b6558
kbenmus02 <mkbensmaia@gmail.com>
2022-01-19 16:57:01 -0500

    updates

--- cours03_flask_file/app.py
+++ cours03_flask_file/app.py
@@ -196,9 +196,6 @@ def load_model(model_file_info: str):
     #--- Charge le modèle
 
 
-
-
-
 if __name__ == "__main__":
     port = os.environ.get("PORT", 5000)
-    app.run(debug=False, host="0.0.0.0", port = port)
+    app.run(debug=False, host="0.0.0.0", port = por
